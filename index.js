0177f2aa58bb9b8b135ca40a005764e9de80cfea
Vishrut Patel <vrp101@gmail.com>
2018-08-27 18:46:46 +0530

    survey creation setting up

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const keys = require('./config/keys');
 
 mongoose.connect(keys.mongoURI);
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 const app = express();
@@ -23,6 +24,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   // serve production assets files (main.js, main.css...)
