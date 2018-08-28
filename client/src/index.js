d01d32c4804f93c2d585803b6343f56ee981850f
Vishrut Patel <vrp101@gmail.com>
2018-08-28 15:24:54 +0530

    create backend for survey creation and sending emails

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,9 @@ import { createStore, applyMiddleware } from 'redux';
 import App from './components/App';
 import reducers from './reducers';
 
+import axios from 'axios';
+window.axios = axios;
+
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
